22eac1e207ffe67afac3073e47f309f61e19fbc4
Shazeen Fabius <shazeenfabius@Shazeens-MBP.lan>
2021-03-11 19:34:49 -0800

    set up tostring

--- src/main/java/com/lambdaschool/countries/models/Country.java
+++ src/main/java/com/lambdaschool/countries/models/Country.java
@@ -28,4 +28,55 @@ public class Country
         this.landmasskm2 = landmasskm2;
         this.medianage = medianage;
     }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public long getPopulation()
+    {
+        return population;
+    }
+
+    public void setPopulation(long population)
+    {
+        this.population = population;
+    }
+
+    public long getLandmasskm2()
+    {
+        return landmasskm2;
+    }
+
+    public void setLandmasskm2(long landmasskm2)
+    {
+        this.landmasskm2 = landmasskm2;
+    }
+
+    public int getMedianage()
+    {
+        return medianage;
+    }
+
+    public void setMedianage(int medianage)
+    {
+        this.medianage = medianage;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Country{" +
+            "name='" + name + '\'' +
+            ", population=" + population +
+            ", landmasskm2=" + landmasskm2 +
+            ", medianage=" + medianage +
+            '}';
+    }
 }
